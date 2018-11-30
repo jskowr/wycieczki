869cbd74f958c06c49fb00f9876d1b88c4e7431f
KPiskorz196 <45358556+KPiskorz196@users.noreply.github.com>
2018-11-30 18:19:58 +0100

    Show all data, display 90% of addon form

--- modules/protectors.php
+++ modules/protectors.php
@@ -4,6 +4,11 @@ switch($link[2]) {
 
     case 'add':
         if($_POST['action'] == 'add'){
+            if($core -> addClass($_POST['nazwisko'], $_POST['imie'], $_POST['adres'], $_POST['kod_pocztowy'],$_POST['pesel'],$_POST['telefon'],$_POST['email'])) {
+                $_SESSION['msg']['m1']['title'] = "Dodano nową klasę.";
+                header('Location: ' .$config['url'] . 'classess');
+                exit();
+            }
         }
 
         break;
