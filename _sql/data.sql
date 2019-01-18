f95715d61e37fd60becefb86d0257c98d5e10f24
KPiskorz196 <45358556+KPiskorz196@users.noreply.github.com>
2019-01-18 13:39:13 +0100

    added login data

--- _sql/data.sql
+++ _sql/data.sql
@@ -1,24 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb4
--- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Czas generowania: 27 Lis 2018, 20:52
--- Wersja serwera: 10.1.26-MariaDB-0+deb9u1
--- Wersja PHP: 7.2.8-1+0~20180725124257.2+stretch~1.gbp571e56
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+-- Zrzut danych tabeli `kierownicy`
 --
--- Baza danych: `wycieczki`
---
+
+INSERT INTO `kierownicy` (`ID`, `login`, `password`, `Nazwisko`, `Imie`, `nr_tel`, `email`) VALUES
+(1, 'kierowniktest1', 'testpass1', 'Testowy', 'Test', 123456789, 'wwwwww@www@ww'),
+(2, 'kierowniktest2', 'ZAQ!2wsx', 'Test', 'hhhhh', 999666444, 'ccv@www.com');
+
+-- --------------------------------------------------------------------------------------------------
 
 --
 -- Zrzut danych tabeli `klasy`
@@ -27,17 +15,20 @@ SET time_zone = "+00:00";
 INSERT INTO `klasy` (`id`, `symbol`) VALUES
 (3, 'Ib'),
 (4, 'IIa'),
-(5, 'IIIb'),
 (6, 'IVc'),
-(8, 'IIa');
+(8, 'IIa'),
+(9, 'IIf');
+
+-- --------------------------------------------------------------------------------------------------
 
 --
 -- Zrzut danych tabeli `klasy_wycieczki`
 --
 
 INSERT INTO `klasy_wycieczki` (`id`, `id_klasy`, `id_wycieczki`) VALUES
-(3, 6, 3),
-(4, 5, 1);
+(3, 6, 3);
+
+-- ---------------------------------------------------------------------------------------------------
 
 --
 -- Zrzut danych tabeli `miejsca`
@@ -50,6 +41,8 @@ INSERT INTO `miejsca` (`id`, `nazwa`, `adres`, `id_miejscowosci`) VALUES
 (4, 'B&B Hotel', 'Leona Szumana 8', 5),
 (5, 'Hotel Skaliny', 'Pardałówka 3b', 2);
 
+-- ---------------------------------------------------------------------------------------------------
+
 --
 -- Zrzut danych tabeli `miejscowosci`
 --
@@ -61,13 +54,17 @@ INSERT INTO `miejscowosci` (`id`, `nazwa`) VALUES
 (4, 'Berlin'),
 (5, 'Wrocław');
 
+-- ---------------------------------------------------------------------------------------------------
+
 --
 -- Zrzut danych tabeli `opiekunowie`
 --
 
-INSERT INTO `opiekunowie` (`id`, `nazwisko`, `imie`, `adres`, `kod_pocztowy`, `pesel`, `telefon`, `email`, `id_miejscowosci`) VALUES
-(1, 'Nowak', 'Mirosław', 'Jęczmienna 104', '53-307', '90100719855', 534768012, 'miroslaw.nowak@gmail.com', 1),
-(2, 'Pietrzak', 'Aleksandra', 'Wojska Polskiego 12', '48-330', '85241109019', 501097145, 'aleksandra.pietrzak@gmail.com', 3);
+INSERT INTO `opiekunowie` (`id`, `nazwisko`, `imie`, `login`, `haslo`, `adres`, `kod_pocztowy`, `pesel`, `telefon`, `email`, `id_miejscowosci`, `id_szkoly`) VALUES
+(1, 'Nowak', 'Mirosław', 'opiekun2', 'ZAQ!2wsx', 'Jęczmienna 104', '53-307', '90100719855', 534768012, 'miroslaw.nowak@gmail.com', 1, 2),
+(2, 'Pietrzak', 'Aleksandra', 'opiekuntest', 'ZXCV', 'Wojska Polskiego 12', '48-330', '85241109019', 501097145, 'aleksandra.pietrzak@gmail.com', 3, 1);
+
+-- ----------------------------------------------------------------------------------------------------
 
 --
 -- Zrzut danych tabeli `opiekunowie_klas`
@@ -76,8 +73,9 @@ INSERT INTO `opiekunowie` (`id`, `nazwisko`, `imie`, `adres`, `kod_pocztowy`, `p
 INSERT INTO `opiekunowie_klas` (`id`, `id_opiekuna`, `id_klasy`) VALUES
 (1, 1, 3),
 (2, 1, 4),
-(3, 2, 6),
-(4, 2, 5);
+(3, 2, 6);
+
+-- ----------------------------------------------------------------------------------------------------
 
 --
 -- Zrzut danych tabeli `przewodnicy`
@@ -86,6 +84,8 @@ INSERT INTO `opiekunowie_klas` (`id`, `id_opiekuna`, `id_klasy`) VALUES
 INSERT INTO `przewodnicy` (`id`, `nazwisko`, `imie`, `adres`, `kod_pocztowy`, `pesel`, `telefon`, `email`, `id_miejscowosci`) VALUES
 (1, 'Wróbel', 'Krzysztof', 'Mickiewicza 21', '51-304', '86311910514', 456098123, 'wrobel.krzysztof@gmail.com', 5),
 (2, 'Wiśniewski', 'Janusz', 'Piłsudskiego 4', '57-102', '89280119451', 532467834, 'janusz.wisniewski@onet.pl', 4);
+
+-- -----------------------------------------------------------------------------------------------------
 
 --
 -- Zrzut danych tabeli `przewodnicy_miejsc`
@@ -97,6 +97,18 @@ INSERT INTO `przewodnicy_miejsc` (`id`, `id_przewodnika`, `id_miejsca`) VALUES
 (3, 2, 5),
 (4, 2, 2);
 
+-- ------------------------------------------------------------------------------------------------------
+
+--
+-- Zrzut danych tabeli `szkola`
+--
+
+INSERT INTO `szkola` (`ID`, `Nazwa`, `Adres`, `Miasto`) VALUES
+(1, 'Szkoła podstawowa imienia Oblatów NMP', 'Szkolna 17', 5),
+(2, 'Szkoła podstawowa nr 3', 'Konopnickiej 2', 3);
+
+-- ------------------------------------------------------------------------------------------------------
+
 --
 -- Zrzut danych tabeli `uczniowie`
 --
@@ -107,15 +119,15 @@ INSERT INTO `uczniowie` (`id`, `nazwisko`, `imie`, `plec`, `adres`, `kod_pocztow
 (3, 'Adamska', 'Krystyna', 'K', 'Grabiszyńska 8', '53-507', '00221614457', 509213234, 3, 1),
 (4, 'Skotnicka', 'Wiktoria', 'K', 'Narutowicza 3', '80-007', '99100719855', 867234654, 4, 5);
 
+-- ------------------------------------------------------------------------------------------------------
+
 --
 -- Zrzut danych tabeli `wycieczki`
 --
 
-INSERT INTO `wycieczki` (`id`, `nazwa`, `data_rozpoczecia`, `data_zakonczenia`, `liczba_uczniow`, `id_miejscowosci`) VALUES
-(1, 'Zwiedzanie Muzeum Narodowego', '2018-11-22', '2018-11-23', 32, 3),
-(2, 'Zielona szkoła 2018', '2018-11-28', '2018-11-30', 16, 4),
-(3, 'Tropical Island Berlin 2017', '2018-12-27', '2018-12-30', 21, 1);
+INSERT INTO `wycieczki` (`id`, `nazwa`, `data_rozpoczecia`, `data_zakonczenia`, `liczba_uczniow`, `id_miejscowosci`, `id_kierownika`) VALUES
+(1, 'Zwiedzanie Muzeum Narodowego', '2018-11-22', '2018-11-23', 32, 3, 1),
+(2, 'Zielona szkoła 2018', '2018-11-28', '2018-11-30', 16, 4, 2),
+(3, 'Tropical Island Berlin 2017', '2018-12-27', '2018-12-30', 21, 1, 2);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- -------------------------------------------------------------------------------------------------------
--- _sql/structure.sql
+++ _sql/structure.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Czas generowania: 27 Lis 2018, 20:50
--- Wersja serwera: 10.1.26-MariaDB-0+deb9u1
--- Wersja PHP: 7.2.8-1+0~20180725124257.2+stretch~1.gbp571e56
+-- Host: 127.0.0.1
+-- Czas generowania: 18 Sty 2019, 13:27
+-- Wersja serwera: 10.1.36-MariaDB
+-- Wersja PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,6 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `kierownicy`
+--
+
+CREATE TABLE `kierownicy` (
+  `ID` int(11) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `Nazwisko` varchar(50) NOT NULL,
+  `Imie` varchar(50) NOT NULL,
+  `nr_tel` int(9) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `kierownicy`
+--
+
+INSERT INTO `kierownicy` (`ID`, `login`, `password`, `Nazwisko`, `Imie`, `nr_tel`, `email`) VALUES
+(1, 'kierowniktest1', 'testpass1', 'Testowy', 'Test', 123456789, 'wwwwww@www@ww'),
+(2, 'kierowniktest2', 'ZAQ!2wsx', 'Test', 'hhhhh', 999666444, 'ccv@www.com');
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `klasy`
 --
 
@@ -30,6 +56,17 @@ CREATE TABLE `klasy` (
   `id` int(11) NOT NULL,
   `symbol` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `klasy`
+--
+
+INSERT INTO `klasy` (`id`, `symbol`) VALUES
+(3, 'Ib'),
+(4, 'IIa'),
+(6, 'IVc'),
+(8, 'IIa'),
+(9, 'IIf');
 
 -- --------------------------------------------------------
 
@@ -42,6 +79,13 @@ CREATE TABLE `klasy_wycieczki` (
   `id_klasy` int(11) NOT NULL,
   `id_wycieczki` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `klasy_wycieczki`
+--
+
+INSERT INTO `klasy_wycieczki` (`id`, `id_klasy`, `id_wycieczki`) VALUES
+(3, 6, 3);
 
 -- --------------------------------------------------------
 
@@ -56,6 +100,17 @@ CREATE TABLE `miejsca` (
   `id_miejscowosci` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `miejsca`
+--
+
+INSERT INTO `miejsca` (`id`, `nazwa`, `adres`, `id_miejscowosci`) VALUES
+(1, 'Muzeum Narodowe', 'Aleje Jerozolimskie 3', 3),
+(2, 'Hotel Aqua', 'Zamkowa Góra 35', 4),
+(3, 'Tropical Island', 'Tropical-Islands-Allee 1', 1),
+(4, 'B&B Hotel', 'Leona Szumana 8', 5),
+(5, 'Hotel Skaliny', 'Pardałówka 3b', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -67,6 +122,17 @@ CREATE TABLE `miejscowosci` (
   `nazwa` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `miejscowosci`
+--
+
+INSERT INTO `miejscowosci` (`id`, `nazwa`) VALUES
+(1, 'Opole'),
+(2, 'Żmigród'),
+(3, 'Zakopane'),
+(4, 'Berlin'),
+(5, 'Wrocław');
+
 -- --------------------------------------------------------
 
 --
@@ -77,13 +143,24 @@ CREATE TABLE `opiekunowie` (
   `id` int(11) NOT NULL,
   `nazwisko` varchar(25) NOT NULL,
   `imie` varchar(15) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `haslo` varchar(255) NOT NULL,
   `adres` varchar(50) DEFAULT NULL,
   `kod_pocztowy` varchar(6) DEFAULT NULL,
   `pesel` varchar(11) NOT NULL,
   `telefon` int(9) DEFAULT NULL,
   `email` varchar(40) DEFAULT NULL,
-  `id_miejscowosci` int(11) DEFAULT NULL
+  `id_miejscowosci` int(11) DEFAULT NULL,
+  `id_szkoly` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `opiekunowie`
+--
+
+INSERT INTO `opiekunowie` (`id`, `nazwisko`, `imie`, `login`, `haslo`, `adres`, `kod_pocztowy`, `pesel`, `telefon`, `email`, `id_miejscowosci`, `id_szkoly`) VALUES
+(1, 'Nowak', 'Mirosław', 'opiekun2', 'ZAQ!2wsx', 'Jęczmienna 104', '53-307', '90100719855', 534768012, 'miroslaw.nowak@gmail.com', 1, 2),
+(2, 'Pietrzak', 'Aleksandra', 'opiekuntest', 'ZXCV', 'Wojska Polskiego 12', '48-330', '85241109019', 501097145, 'aleksandra.pietrzak@gmail.com', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -96,6 +173,15 @@ CREATE TABLE `opiekunowie_klas` (
   `id_opiekuna` int(11) NOT NULL,
   `id_klasy` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `opiekunowie_klas`
+--
+
+INSERT INTO `opiekunowie_klas` (`id`, `id_opiekuna`, `id_klasy`) VALUES
+(1, 1, 3),
+(2, 1, 4),
+(3, 2, 6);
 
 -- --------------------------------------------------------
 
@@ -115,6 +201,14 @@ CREATE TABLE `przewodnicy` (
   `id_miejscowosci` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `przewodnicy`
+--
+
+INSERT INTO `przewodnicy` (`id`, `nazwisko`, `imie`, `adres`, `kod_pocztowy`, `pesel`, `telefon`, `email`, `id_miejscowosci`) VALUES
+(1, 'Wróbel', 'Krzysztof', 'Mickiewicza 21', '51-304', '86311910514', 456098123, 'wrobel.krzysztof@gmail.com', 5),
+(2, 'Wiśniewski', 'Janusz', 'Piłsudskiego 4', '57-102', '89280119451', 532467834, 'janusz.wisniewski@onet.pl', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -126,6 +220,37 @@ CREATE TABLE `przewodnicy_miejsc` (
   `id_przewodnika` int(11) NOT NULL,
   `id_miejsca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `przewodnicy_miejsc`
+--
+
+INSERT INTO `przewodnicy_miejsc` (`id`, `id_przewodnika`, `id_miejsca`) VALUES
+(1, 1, 2),
+(2, 1, 3),
+(3, 2, 5),
+(4, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `szkola`
+--
+
+CREATE TABLE `szkola` (
+  `ID` int(11) NOT NULL,
+  `Nazwa` varchar(255) NOT NULL,
+  `Adres` varchar(255) NOT NULL,
+  `Miasto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `szkola`
+--
+
+INSERT INTO `szkola` (`ID`, `Nazwa`, `Adres`, `Miasto`) VALUES
+(1, 'Szkoła podstawowa imienia Oblatów NMP', 'Szkolna 17', 5),
+(2, 'Szkoła podstawowa nr 3', 'Konopnickiej 2', 3);
 
 -- --------------------------------------------------------
 
@@ -146,6 +271,16 @@ CREATE TABLE `uczniowie` (
   `id_miejscowosci` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Zrzut danych tabeli `uczniowie`
+--
+
+INSERT INTO `uczniowie` (`id`, `nazwisko`, `imie`, `plec`, `adres`, `kod_pocztowy`, `pesel`, `telefon`, `id_klasy`, `id_miejscowosci`) VALUES
+(1, 'Kowalski', 'Jan', 'M', 'Mickiewicza 19b', '60-134', '01311910514', 872213095, 3, 2),
+(2, 'Nowak', 'Adam', 'M', 'Sienkiewicza 7', '00-014', '01280119451', 837268767, 3, 2),
+(3, 'Adamska', 'Krystyna', 'K', 'Grabiszyńska 8', '53-507', '00221614457', 509213234, 3, 1),
+(4, 'Skotnicka', 'Wiktoria', 'K', 'Narutowicza 3', '80-007', '99100719855', 867234654, 4, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -158,21 +293,37 @@ CREATE TABLE `wycieczki` (
   `data_rozpoczecia` date NOT NULL,
   `data_zakonczenia` date NOT NULL,
   `liczba_uczniow` int(11) NOT NULL,
-  `id_miejscowosci` int(11) DEFAULT NULL
+  `id_miejscowosci` int(11) DEFAULT NULL,
+  `id_kierownika` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `wycieczki`
+--
+
+INSERT INTO `wycieczki` (`id`, `nazwa`, `data_rozpoczecia`, `data_zakonczenia`, `liczba_uczniow`, `id_miejscowosci`, `id_kierownika`) VALUES
+(1, 'Zwiedzanie Muzeum Narodowego', '2018-11-22', '2018-11-23', 32, 3, 1),
+(2, 'Zielona szkoła 2018', '2018-11-28', '2018-11-30', 16, 4, 2),
+(3, 'Tropical Island Berlin 2017', '2018-12-27', '2018-12-30', 21, 1, 2);
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `klasy`
+-- Indeksy dla tabeli `kierownicy`
+--
+ALTER TABLE `kierownicy`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `klasy`
 --
 ALTER TABLE `klasy`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `klasy_wycieczki`
+-- Indeksy dla tabeli `klasy_wycieczki`
 --
 ALTER TABLE `klasy_wycieczki`
   ADD PRIMARY KEY (`id`),
@@ -180,27 +331,28 @@ ALTER TABLE `klasy_wycieczki`
   ADD KEY `id_wycieczki` (`id_wycieczki`);
 
 --
--- Indexes for table `miejsca`
+-- Indeksy dla tabeli `miejsca`
 --
 ALTER TABLE `miejsca`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_miejscowosci` (`id_miejscowosci`);
 
 --
--- Indexes for table `miejscowosci`
+-- Indeksy dla tabeli `miejscowosci`
 --
 ALTER TABLE `miejscowosci`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `opiekunowie`
+-- Indeksy dla tabeli `opiekunowie`
 --
 ALTER TABLE `opiekunowie`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_miejscowosci` (`id_miejscowosci`);
+  ADD KEY `id_miejscowosci` (`id_miejscowosci`),
+  ADD KEY `id_szkoly` (`id_szkoly`);
 
 --
--- Indexes for table `opiekunowie_klas`
+-- Indeksy dla tabeli `opiekunowie_klas`
 --
 ALTER TABLE `opiekunowie_klas`
   ADD PRIMARY KEY (`id`),
@@ -208,14 +360,14 @@ ALTER TABLE `opiekunowie_klas`
   ADD KEY `id_klasy` (`id_klasy`);
 
 --
--- Indexes for table `przewodnicy`
+-- Indeksy dla tabeli `przewodnicy`
 --
 ALTER TABLE `przewodnicy`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_miejscowosci` (`id_miejscowosci`);
 
 --
--- Indexes for table `przewodnicy_miejsc`
+-- Indeksy dla tabeli `przewodnicy_miejsc`
 --
 ALTER TABLE `przewodnicy_miejsc`
   ADD PRIMARY KEY (`id`),
@@ -223,7 +375,13 @@ ALTER TABLE `przewodnicy_miejsc`
   ADD KEY `id_miejsca` (`id_miejsca`);
 
 --
--- Indexes for table `uczniowie`
+-- Indeksy dla tabeli `szkola`
+--
+ALTER TABLE `szkola`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `uczniowie`
 --
 ALTER TABLE `uczniowie`
   ADD PRIMARY KEY (`id`),
@@ -231,66 +389,89 @@ ALTER TABLE `uczniowie`
   ADD KEY `id_miejscowosci` (`id_miejscowosci`);
 
 --
--- Indexes for table `wycieczki`
+-- Indeksy dla tabeli `wycieczki`
 --
 ALTER TABLE `wycieczki`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_miejscowosci` (`id_miejscowosci`);
+  ADD KEY `id_miejscowosci` (`id_miejscowosci`),
+  ADD KEY `id_kierownika` (`id_kierownika`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `kierownicy`
+--
+ALTER TABLE `kierownicy`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT dla tabeli `klasy`
 --
 ALTER TABLE `klasy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT dla tabeli `klasy_wycieczki`
 --
 ALTER TABLE `klasy_wycieczki`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT dla tabeli `miejsca`
 --
 ALTER TABLE `miejsca`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT dla tabeli `miejscowosci`
 --
 ALTER TABLE `miejscowosci`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT dla tabeli `opiekunowie`
 --
 ALTER TABLE `opiekunowie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT dla tabeli `opiekunowie_klas`
 --
 ALTER TABLE `opiekunowie_klas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT dla tabeli `przewodnicy`
 --
 ALTER TABLE `przewodnicy`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT dla tabeli `przewodnicy_miejsc`
 --
 ALTER TABLE `przewodnicy_miejsc`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `szkola`
+--
+ALTER TABLE `szkola`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT dla tabeli `uczniowie`
 --
 ALTER TABLE `uczniowie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT dla tabeli `wycieczki`
 --
 ALTER TABLE `wycieczki`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- Ograniczenia dla zrzutów tabel
 --
@@ -312,7 +493,8 @@ ALTER TABLE `miejsca`
 -- Ograniczenia dla tabeli `opiekunowie`
 --
 ALTER TABLE `opiekunowie`
-  ADD CONSTRAINT `opiekunowie_ibfk_1` FOREIGN KEY (`id_miejscowosci`) REFERENCES `miejscowosci` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `opiekunowie_ibfk_1` FOREIGN KEY (`id_miejscowosci`) REFERENCES `miejscowosci` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `opiekunowie_ibfk_2` FOREIGN KEY (`id_szkoly`) REFERENCES `szkola` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ograniczenia dla tabeli `opiekunowie_klas`
@@ -345,7 +527,9 @@ ALTER TABLE `uczniowie`
 -- Ograniczenia dla tabeli `wycieczki`
 --
 ALTER TABLE `wycieczki`
-  ADD CONSTRAINT `wycieczki_ibfk_1` FOREIGN KEY (`id_miejscowosci`) REFERENCES `miejscowosci` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `wycieczki_ibfk_1` FOREIGN KEY (`id_miejscowosci`) REFERENCES `miejscowosci` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `wycieczki_ibfk_2` FOREIGN KEY (`id_kierownika`) REFERENCES `kierownicy` (`ID`) ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
